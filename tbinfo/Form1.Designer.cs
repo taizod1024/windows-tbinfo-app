4dabc406444620312f639a2ed2d39819bfa6e560
yamamoto.taizo <taizod1024@gmail.com>
2025-04-15 00:08:25 +0900

    初期コミット

---
+++ tbinfo/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace TbInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.uSERDOMAINToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOMPUTERNAMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uSERNAMEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iPV4ADDRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iPV6ADDRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.customTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(18, 2);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "text1 text2 text3 text4 text5 text6 text7 text8 text9";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uSERDOMAINToolStripMenuItem,
+            this.cOMPUTERNAMEToolStripMenuItem,
+            this.uSERNAMEToolStripMenuItem,
+            this.iPV4ADDRToolStripMenuItem,
+            this.iPV6ADDRToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.customTextToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(170, 170);
+            // 
+            // uSERDOMAINToolStripMenuItem
+            // 
+            this.uSERDOMAINToolStripMenuItem.Name = "uSERDOMAINToolStripMenuItem";
+            this.uSERDOMAINToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.uSERDOMAINToolStripMenuItem.Text = "USERDOMAIN";
+            // 
+            // cOMPUTERNAMEToolStripMenuItem
+            // 
+            this.cOMPUTERNAMEToolStripMenuItem.Name = "cOMPUTERNAMEToolStripMenuItem";
+            this.cOMPUTERNAMEToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.cOMPUTERNAMEToolStripMenuItem.Text = "COMPUTERNAME";
+            // 
+            // uSERNAMEToolStripMenuItem
+            // 
+            this.uSERNAMEToolStripMenuItem.Name = "uSERNAMEToolStripMenuItem";
+            this.uSERNAMEToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.uSERNAMEToolStripMenuItem.Text = "USERNAME";
+            // 
+            // iPV4ADDRToolStripMenuItem
+            // 
+            this.iPV4ADDRToolStripMenuItem.Name = "iPV4ADDRToolStripMenuItem";
+            this.iPV4ADDRToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.iPV4ADDRToolStripMenuItem.Text = "IPV4 ADDR";
+            // 
+            // iPV6ADDRToolStripMenuItem
+            // 
+            this.iPV6ADDRToolStripMenuItem.Name = "iPV6ADDRToolStripMenuItem";
+            this.iPV6ADDRToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.iPV6ADDRToolStripMenuItem.Text = "IPV6 ADDR";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(166, 6);
+            // 
+            // customTextToolStripMenuItem
+            // 
+            this.customTextToolStripMenuItem.Name = "customTextToolStripMenuItem";
+            this.customTextToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.customTextToolStripMenuItem.Text = "custom text";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(166, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.ContextMenuStrip = this.contextMenuStrip1;
+            this.label2.Location = new System.Drawing.Point(4, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 40);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(160, 48);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "tbinfo";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem uSERDOMAINToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uSERNAMEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem customTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOMPUTERNAMEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iPV4ADDRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iPV6ADDRToolStripMenuItem;
+    }
+}
+
---
+++ tbinfo/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TbInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Top = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height - 48;
+            this.timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.TopMost = false;
+            this.TopMost = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
